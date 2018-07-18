d76ac9703331293d7816618be881708bbec56a28
Roshan Choxi <roshan.choxi@gmail.com>
2018-07-18 15:23:08 -0500

    clean up boilerplate

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,16 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './App.scss'
-import react from '../images/reactjs.png'
-import parcel from '../images/parceljs.png'
 
-class App extends Component {
-  static defaultProps = {
-    title: 'React App with Parcel Bundler'
-  }
-
-  state = {
-    isAwesome: true
-  }
-
+export default class App extends Component {
   render() {
-    const awesome = this.state.isAwesome ? <span>awesome</span> : ''
-
     return (
-      <div className="app">
-        <h1 className="title">{this.props.title}</h1>
-
-        <p className="text">
-          Let's make something {awesome} together. Edit{' '}
-          <code>src/components/App.js</code> and save to see changes.
-        </p>
-
-        <img src={react} className="img logo" alt="React.js" />
-
-        <img src={parcel} className="img" alt="Parcel.js" />
+      <div className="Twain">
+        <div className="Jumbotron">
+          <h1>Twain</h1>
+          <h2>Get your copy reviewed by a professional writer.</h2>
+        </div>
       </div>
     )
   }
 }
-
-App.propTypes = {
-  title: PropTypes.string
-}
-
-export default App
