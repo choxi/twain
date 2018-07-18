c7fd7694e124dced76cf93fab41d5554dda395d1
Roshan Choxi <roshan.choxi@gmail.com>
2018-07-18 16:04:09 -0500

    Add features section

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,33 @@ export default class App extends Component {
           <h1 className="Header Header--title">Twain</h1>
           <h2 className="Header Header--secondary pull-up">Professional copy-editing as a service</h2>
         </div>
+
+        <div className="Section Section--alt">
+          <p class="Text--quote">
+            The difference between the right word and the almost right word
+            is the difference between lightning and a lightning bug.
+          </p>
+          <p class="Text--quote">
+            &mdash; Mark Twain
+          </p>
+        </div>
+
+        <div className="Section">
+          <div className="Features">
+            <div className="Features__feature">
+              <div className="Badge">1</div>
+              <p>Submit a draft to Twain to be reviewed by a professional copy editor.</p>
+            </div>
+            <div className="Features__feature">
+              <div className="Badge">2</div>
+              <p>Our editors will review your draft for spelling, grammar, clarity, conciseness, style, and structure.</p>
+            </div>
+            <div className="Features__feature">
+              <div className="Badge">3</div>
+              <p>Receive a draft with highlighted comments and annotated feedback.</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
